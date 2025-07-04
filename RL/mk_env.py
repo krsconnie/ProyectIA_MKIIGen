2d04d8707985443644589dc648e200408ffd74b8
Angie161 <aramirez2022@udec.cl>
2025-07-03 23:43:27 -0400

    Update antes del reseteo

--- RL/mk_env.py
+++ RL/mk_env.py
@@ -5,7 +5,7 @@ import cv2  # OpenCV para redimensionar imágenes
 from gymnasium import spaces
 
 class MortalKombatEnv(gym.Env):
-    def __init__(self, resize_shape=(64, 64), n=4):
+    def __init__(self, resize_shape=(84, 84), n=4):
         super().__init__()
         self.posibles_estados = [f"VeryEasy.LiuKang-{i:02d}" for i in range(2, 4)]
         self.env = None
@@ -59,7 +59,6 @@ class MortalKombatEnv(gym.Env):
 
     # Método de reset del entorno y las variables de recompensa
     def reset(self, seed=None, options=None):
-        #print("RESEEEEEEEEET")
         # Cierra el entorno anterior si existe
         if self.env:
             self.env.close()
@@ -133,11 +132,14 @@ class MortalKombatEnv(gym.Env):
         # Movement_buttons = UP, DOWN, LEFT, RIGHT
         movement_buttons = [4, 5, 6, 7]
 
+        #Añadida nueva variable para atacar. Solo uno debe estar activo, sino no funcionan los ataques
+        attack_pressed = [action[i] for i in attack_buttons]
+
         # Por cada step que genere daño, recompensa base de 0.2 más combo bonus
         if damage_to_Enemy > 0:
             self.combo_golpes_consecutivos += 1
             bonus_combo = min(0.05 * self.combo_golpes_consecutivos, 0.15)
-            reward += 0.35 + bonus_combo + (damage_to_Enemy/120)
+            reward += 0.45 + bonus_combo + (damage_to_Enemy/120)
             self.efective_attack_steps += 1
         else:
             self.combo_golpes_consecutivos = 0
@@ -146,38 +148,35 @@ class MortalKombatEnv(gym.Env):
         if damage_to_Player > 0:
             self.damage_to_player_steps += 1
             reward -= 0.06 + (damage_to_Player/120)
-            if any(action[i] for i in block_buttons):
-                reward += 0.02
+            if any(action[i] for i in block_buttons) and sum(attack_pressed) == 0:
+                #Tentativo a cambia a 0.01 si la IA se vuelve muy defensiva
+                reward += 0.008
                 self.efective_block_steps += 1
 
         # Si golpea, se le recompensa un poco aunque no genere daño, pero debe estar a una distancia de 15
-        if any(action[i] for i in (attack_buttons)) and distancia_actual <= 15 and p1_health_actual == self.p1_health_anterior:
-            #print("RECOMPENSA POR ATACAR EN CUALQUIER MOMENTO: 0.01")
-            reward += 0.03
+        if sum(attack_pressed) == 1 and distancia_actual <= 15 and p1_health_actual == self.p1_health_anterior:
+            reward += 0.6
         
         # Si el jugador se acerca al oponente, se le recompensa
         if distancia_actual <= self.distancia_anterior:
             if (x < x_enemy and action[7] == 1) or (x > x_enemy and action[6] == 1):
-                #print(f"Recompensa por acercase {(self.distancia_anterior - distancia_actual) * 0.0025}")
                 reward += (self.distancia_anterior - distancia_actual) * 0.002 #El cambio de distancias max es desde 199 a 0, lo cual da recompensa max de 0.199
         
         # Si el jugador se aleja del oponente, sin intensión de apartarse de golpes, se le castiga 
         else:
             if self.p1_health_anterior == p1_health_actual and ((x < x_enemy and action[6] == 1) or (x > x_enemy and action[7] == 1)):
-                #print(f"Castigo por alejarse {(distancia_actual - self.distancia_anterior) * -0.002}")
                 reward -= (distancia_actual - self.distancia_anterior) * 0.0018 #El cambio de distancias max es desde 199 a 0, lo cual da recompensa max de 0.2985
 
-        # Si el personaje solo repite las mismas acciones y no produce ningún efecto, entonces será castigado tras una serie 
-        # de ellos. Se considerará solo si al personaje no se le hace daño, ya que puede estar inmovil debido a los golpes
+        # Si el personaje no produce ningún efecto, entonces será castigado tras una serie de pasos. 
+        # Se considerará solo si al personaje no se le hace daño, ya que puede estar inmovil debido a los golpes
         if p1_health_actual == self.p1_health_anterior:
-            if any(action[i] for i in (movement_buttons)) and damage_to_Enemy == 0:
+            if damage_to_Enemy == 0:
                 self.acciones_repetidas += 1
             else:
                 self.acciones_repetidas = 0
 
             if self.acciones_repetidas >= 30:
-                reward -= min(0.05 * (self.acciones_repetidas - 29), 0.4)
-                print(f"Castigo por pasivo {-min(0.05 * (self.acciones_repetidas - 29), 0.4)}")
+                reward -= min(0.05 * (self.acciones_repetidas - 29), 0.5)
 
         # Si el personaje se queda en el mismo sitio por mucho tiempo, sin que le hagan daño, 
         # tendrá que moverse o será castigado. Con esto se busca corregir el comportamiento de quedarse pegado al fondo del mapa
@@ -186,11 +185,10 @@ class MortalKombatEnv(gym.Env):
             if self.last_step_info.get('x_position', 0) == x:
                 self.steps_estatico += 1
             else:
-                print(f"Castigo por estático: 0")
                 self.steps_estatico = 0
-            if self.steps_estatico >= 25:
-                reward -= min(0.03 * (self.steps_estatico - 24), 0.3)
-                print(f"Castigo por estático: {-min(0.03 * (self.steps_estatico - 24), 0.3)}")
+            if self.steps_estatico >= 40:
+                #Cambio de 1 a 0.06
+                reward -= min(0.02 * (self.steps_estatico - 39), 0.04)
         
         if distancia_actual < 5:
             self.steps_cerca_del_enemigo += 1
@@ -203,16 +201,13 @@ class MortalKombatEnv(gym.Env):
         self.last_step_info = info
         
         if terminated or truncated:
-                #
-                # print(f"p1 = {p1_health_actual}, p2 ={p2_health_actual}")
                 #Personaje perdió
                 if p1_health_actual <= 0:
                     reward -= 1
                 #Personaje ganó
                 if p2_health_actual <= 0:
                     reward += 1
-    
-        #print(f"REWARD: {reward}")
+
 
         info["efective_attack_steps"] = self.efective_attack_steps
         info["efective_block_steps"] = self.efective_block_steps
