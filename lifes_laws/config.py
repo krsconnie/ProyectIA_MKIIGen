7d1499ce22166c97d306b5b6768269109a815c1d
Luciano <luargomedo2021@inf.udec.cl>
2025-07-06 17:14:24 -0400

    a

--- lifes_laws/config.py
+++ lifes_laws/config.py
@@ -14,3 +14,28 @@ NOMBRE_MEJOR_AGENTE         = "mejor_agente"
 # Otras constantes
 
 BOTONES_USADOS              = ['A', 'B', 'C', 'START', 'UP', 'DOWN', 'LEFT', 'RIGHT']
+
+# ================================
+# Definiciones de MOVIMIENTOS y COMBOS
+# ================================
+
+MOVIMIENTOS_BASE = {
+    "q": [0,0,0,0,0,0,0,0],  # Quieto
+    "s": [0,0,0,0,1,0,0,0],  # Saltar
+    "a": [0,0,0,0,0,1,0,0],  # Agacharse
+    "i": [0,0,0,0,0,0,1,0],  # Caminar izquierda
+    "d": [0,0,0,0,0,0,0,1],  # Caminar derecha
+    "si": [0,0,0,0,1,0,1,0], # Saltar izquierda
+    "sd": [0,0,0,0,1,0,0,1], # Saltar derecha
+    "b": [0,0,0,1,0,0,0,0],  # Bloquear
+    "ba": [0,0,0,1,0,1,0,0], # Bloquear agachado
+    "pa": [0,0,1,0,0,0,0,0], # Patada alta
+    "pn": [0,1,0,0,0,0,0,0], # Patada normal
+    "gn": [1,0,0,0,0,0,0,0], # Golpe normal
+}
+
+COMBOS_JAX = [
+    ["d", "d", "gn"],             # Agarre de fuerza
+    ["d", "a", "i", "pa"],        # Onda de energía
+    ["a", "gn"]                   # Golpe martillo
+]
--- main.py
+++ main.py
@@ -7,9 +7,9 @@ life.help()
 
 
 # EVOLUCION
-#life.let_there_be_life(config_file="config-neat")
+life.let_there_be_life(config_file="config-neat")
 
 
 # TESTS
 #test.jugar_humano(mapa="LiuKangVsLiuKang_VeryHard_06")
-test.jugar_agente(genoma_file="generaciones_v7/agente.pkl" ,mapa="Level1.JaxVsLiuKang")
+#test.jugar_agente(genoma_file="generaciones_v7/agente.pkl" ,mapa="Level1.JaxVsLiuKang")
